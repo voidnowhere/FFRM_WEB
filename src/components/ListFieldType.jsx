4f257a3033ee5b1e56e00111bd356f5799eef710
AB-Akram <abdelhakakram79@gmail.com>
2023-04-13 12:52:49 +0200

    CRUD web

--- src/components/ListFieldType.jsx
+++ src/components/ListFieldType.jsx
@@ -4,113 +4,251 @@ import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import { Form } from "react-bootstrap";
 import { Modal } from "react-bootstrap";
-export default function ListFieldType (){
-    const [data, setData] = useState([])
+import {Notify} from "notiflix/build/notiflix-notify-aio";
 
-    const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
-    useEffect(() => {
-        axiosInstance.get('api/fieldType').then((response) => {
-        setData(response.data)
+export default function ListFieldType (props){
+
+  const [data, setData] = useState([])
+  
+  const [name, setName] = useState('');
+  const [max, setMax] = useState('');
+  const [priceHour, setPriceHour] = useState('');
+  const [id, setId] = useState('')
         
-    })
+  const [showPost, setShowPost] = useState(false);
+  const handleClosePost = () => setShowPost(false);
+  const handleShowPost = () => setShowPost(true);
 
-    }, [])
-
-    const handleSubmit= event =>{
-      document.getElementById("h1").innerHTML="text" ;
+  const [showEdit, setShowEdit] = useState(false);
+  const handleCloseEdit = () => setShowEdit(false);
+  const handleShowEdit = (e) => {
+    setShowEdit(true);
+    const dataValue = e.target.value;
+    {data.map((data , i) => (
+     data.id == dataValue ? (setName(data.name),setMax(data.max), setPriceHour(data.priceHour), setId(dataValue)):null
+))}
   }
+
+  
+
+  useEffect(() => {
+      axiosInstance.get('api/fieldType').then((response) => {
+      setData(response.data); 
+  })
+
+  }, [])
+
+  
+        const handleSubmit = event =>{
+          event.preventDefault();
+          axiosInstance.post('api/fieldType', {
+              name: name,
+              max: max,
+              priceHour: priceHour
+             
+          }).then(() => {
+            Notify.success("successfully added ", {
+              'position': "center-bottom",
+          });
+          
+            axiosInstance.get('api/fieldType').then((response) => {
+              setData(response.data); })
+          });
     
-    return(
+          
+        setShowPost(false)
+        setName("")
+        setMax("")
+        setPriceHour("")
+      }
 
-      <div className="Container">
-                    <Table striped bordered hover>
-                      
-                    <thead>
-                      <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Max</th>
-                        <th>Price</th>
-                        <th>Advance</th>
-                        <th><Button variant="primary" onClick={handleShow}>Add</Button></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((data , i) => (
-                                <tr key={i}>
-                                <td>{data.id}</td>
-                                <td>{data.name}</td>
-                                <td>{data.max}</td>
-                                <td>{data.price}</td>
-                                <td>{data.advance}</td>
-                                <td><Button variant="success">Edit</Button><Button variant="danger">Delete</Button></td>
-                            </tr>
-                        ))}
-                      
-                    
-                    </tbody>
-                  </Table>
 
-            <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Add FootBall Field Type</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form method="POST" onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="NAME"
-                    autoFocus
-                  />
+      const handlePut = event =>{
+        event.preventDefault();
+        axiosInstance.put('api/fieldType/'+id, {
+            name: name,
+            max: max,
+            priceHour: priceHour
+           
+        }).then(() => {
+          Notify.success("successfully edited ", {
+            'position': "center-bottom",
+        });
+        
+          axiosInstance.get('api/fieldType').then((response) => {
+            setData(response.data); })
+        });
+  
+        
+      setShowEdit(false)
+      setName("")
+      setMax("")
+      setPriceHour("")
+    }
 
-                </Form.Group>
+    const handleDelete = event =>{
+      setId(event.target.value)
+      axiosInstance.delete('api/fieldType/'+id) 
+         .then(() => {
+        Notify.success("successfully deleted ", {
+          'position': "center-bottom",
+      });
+      
+        axiosInstance.get('api/fieldType').then((response) => {
+          setData(response.data); })
+      });
 
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                  <Form.Label>Max</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="MAX"
-                    
-                  />
-                  
-                </Form.Group>
+  }
 
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-                  <Form.Label>Price</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="PRICE"
-                    
-                  />
-                  
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-                  <Form.Label>Advance</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="ADVANCE"
-                    
-                  />
-                  
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit" onClick={handleClose}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
-            </Modal>
-        </div>
-        );
+      
+return(
+     
+    <div className="Container">
+     
+     
+    <Table striped bordered hover>
+      
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Max</th>
+        <th>Price Hour</th>
+        <th><Button variant="primary" onClick={handleShowPost}>Add</Button></th>
+      </tr>
+    </thead>
+    <tbody>
+        {data.map((data , i) => (
+                <tr key={i}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.max}</td>
+                <td>{data.priceHour}</td>
+                <td ><Button value={data.id} variant="success" onClick={handleShowEdit}>Edit</Button><Button variant="danger" value={data.id} onClick={handleDelete}>Delete</Button></td>
+            </tr>
+        ))}
+      
+    
+    </tbody>
+  </Table>
+
+
+<Modal show={showPost} onHide={handleClosePost}>
+<Modal.Header closeButton>
+<Modal.Title>Add FootBall Field Type</Modal.Title>
+</Modal.Header>
+<Modal.Body>
+<Form  method="POST" onSubmit={(handleSubmit)}>
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+  <Form.Label>Name</Form.Label>
+  <Form.Control id="inputNAME"
+    type="text"
+    placeholder="NAME"
+    value={name}
+   onChange={(e) =>setName(event.target.value)}
+    autoFocus
+    
+  />
+
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+  <Form.Label>Max</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="MAX"
+    value={max}
+    onChange={(e) =>setMax(event.target.value)}
+    
+  />
+  
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+  <Form.Label>Price</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="PRICE Hour"
+    value={priceHour}
+    onChange={(e) =>setPriceHour(event.target.value)}
+  />
+  
+</Form.Group>
+
+
+<Modal.Footer>
+<Button variant="secondary" onClick={handleClosePost}>
+Cancel
+</Button>
+<Button variant="primary" type="submit" >
+Add
+</Button>
+</Modal.Footer>
+</Form>
+</Modal.Body>
+</Modal>
+
+
+
+
+
+<Modal show={showEdit} onHide={handleCloseEdit}>
+<Modal.Header closeButton>
+<Modal.Title>Edit FootBall Field Type</Modal.Title>
+</Modal.Header>
+<Modal.Body>
+<Form  method="PUT" onSubmit={(handlePut)}>
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+  <Form.Label>Name</Form.Label>
+  <Form.Control 
+    type="text"
+    placeholder="NAME"
+   value={name}
+   onChange={(e) =>setName(event.target.value)}
+    autoFocus
+    id="inputNameEdit"
+  />
+
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+  <Form.Label>Max</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="MAX"
+    value={max}
+    onChange={(e) =>setMax(event.target.value)}
+    
+  />
+  
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+  <Form.Label>Price</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="PRICE Hour"
+    value={priceHour}
+    onChange={(e) =>setPriceHour(event.target.value)}
+  />
+  
+</Form.Group>
+
+
+<Modal.Footer>
+<Button variant="secondary" onClick={handleCloseEdit}>
+Cancel
+</Button>
+<Button variant="primary" type="submit" >
+Edit
+</Button>
+</Modal.Footer>
+</Form>
+</Modal.Body>
+</Modal>
+    </div>
+);
+
 }
