5de4b4b8cd0a7f27dd13c601d24879ffcf4757cd
voidnowhere <alivnw@outlook.com>
2023-04-16 23:32:16 +0000

    Check if reservation can be paid before processing payment

--- src/components/TempReservations.jsx
+++ src/components/TempReservations.jsx
@@ -5,6 +5,9 @@ import axiosInstance from "../axiosInstance.js";
 import {loadStripe} from "@stripe/stripe-js";
 import {Elements} from "@stripe/react-stripe-js";
 import CheckoutForm from "./Checkout/CheckoutForm.jsx";
+import {Report} from 'notiflix/build/notiflix-report-aio';
+import {Confirm} from 'notiflix/build/notiflix-confirm-aio';
+
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
@@ -21,11 +24,32 @@ function TempReservations() {
     }, []);
 
     function initPayment(reservationId) {
-        axiosInstance.get(`api/temp_reservations/${reservationId}/payment/`).then(response => {
-            setClientSecret(response.data.client_secret);
-            setCurrentReservationId(reservationId);
-            setShowPaymentModal(true);
-        });
+        Confirm.show(
+            'Confirm',
+            'Do you really want to pay ?',
+            'Yes',
+            'No',
+            () => {
+                axiosInstance.get(`api/temp_reservations/${reservationId}/payment/`).then(response => {
+                    setClientSecret(response.data.client_secret);
+                    setCurrentReservationId(reservationId);
+                    setShowPaymentModal(true);
+                }).catch(error => {
+                    setReservations(reservations.map(reservation => {
+                        if (reservation.id === reservationId) {
+                            reservation.can_pay = false;
+                        }
+                        return reservation;
+                    }));
+                    Report.failure(
+                        'Failure',
+                        error.response.data.message,
+                        'Okay',
+                        {backOverlay: false}
+                    );
+                });
+            },
+        );
     }
 
     return (
@@ -52,16 +76,23 @@ function TempReservations() {
                             <td>{reservation.begin_time.slice(0, -3)}{' '}{reservation.end_time.slice(0, -3)}</td>
                             <td>{reservation.price_to_pay} DH</td>
                             <td>
-                                {
-                                    (reservation.is_paid) ?
-                                        <Badge bg="success">Paid</Badge>
-                                        :
-                                        <Badge bg="danger">Unpaid</Badge>
-                                }
+                                <div className="d-flex flex-wrap align-items-stretch gap-2">
+                                    {
+                                        (reservation.is_paid) ?
+                                            <Badge bg="success">Paid</Badge>
+                                            :
+                                            <Badge bg="danger">Unpaid</Badge>
+                                    }
+                                    {
+                                        !reservation.can_pay
+                                        &&
+                                        <Badge bg="danger">Unavailable</Badge>
+                                    }
+                                </div>
                             </td>
                             <td>
                                 {
-                                    !reservation.is_paid
+                                    !reservation.is_paid && reservation.can_pay
                                     &&
                                     <Button variant="outline-primary" size="sm"
                                             onClick={() => initPayment(reservation.id)}
