89460209b4adbfa5f710d8f28c8a6074e4a246ac
voidnowhere <alivnw@outlook.com>
2023-05-12 07:56:28 +0100

    revert vercel analytics to old place

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {Provider} from "react-redux";
 import store from "./store.js";
-import {inject} from '@vercel/analytics';
-
-inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
